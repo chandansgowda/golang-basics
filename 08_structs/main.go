0a58e4302fb55b6b739c8954b7eab37d9fe96e79
chandansgowda <chandansuresh007@gmail.com>
2023-10-05 21:25:32 +0530

    added methods

--- 08_structs/main.go
+++ 08_structs/main.go
@@ -4,10 +4,15 @@ import "fmt"
 
 func main(){
 
-	chandan := User{"Chandan", true, 21, "CSE"};
+	chandan := User{"Chandan", true, 21, "CSE", "c@c.com"};
 
 	fmt.Println(chandan);
 	fmt.Println(chandan.Name, "is from", chandan.Department, "department");
+
+	chandan.GetPermissionStatus();
+
+	chandan.UpdateEmail("c@new.com");
+	fmt.Println(chandan);
 
 }
 
@@ -16,4 +21,17 @@ type User struct {
 	IsAdmin bool
 	Age int16
 	Department string
+	Email string
+}
+
+func (u User) GetPermissionStatus() {
+	if u.IsAdmin {
+		fmt.Println("User is Admin")
+	} else{
+		fmt.Println("User is not an admin")
+	}
+}
+
+func (u *User) UpdateEmail(newEmail string) {
+	u.Email = newEmail;
 }
